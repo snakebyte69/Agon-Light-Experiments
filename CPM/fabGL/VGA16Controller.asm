2ffec14e099742f8136c92d2836a273fab5dd719
Snake <39090848+snakebyte69@users.noreply.github.com>
2023-03-10 22:54:35 +1100

    Add files via upload
    Attempt at sending fabGL commands from the eZ80 under CP/M to the ESP32 via SPI

---
+++ CPM/fabGL/VGA16Controller.asm
@@ -0,0 +1,40 @@
+; Initialize SPI port
+LD A, 0b00000100  ; Enable SPI
+OUT (0x01), A     ; Write to control register
+LD A, 0b00000011  ; Set SPI clock speed to 1MHz
+OUT (0x02), A     ; Write to clock register
+
+; Initialize FabGL command
+LD HL, cmd_vga16 ; Load address of command buffer
+LD DE, spi_buffer ; Load address of SPI buffer
+LD BC, cmd_length ; Load command buffer length
+
+; Send command via SPI
+ld a, (bc)
+ld b, 0
+ld c, a
+ld a, (de)
+rst 0x38          ; Send byte via SPI
+inc bc
+inc de
+djnz $-7         ; Loop for the rest of the bytes
+
+; Exit program
+RET
+
+; Command buffer for fabgl::VGA16Controller
+cmd_vga16:
+.DB 0x04, 0x00     ; Command code for fabgl::VGA16Controller
+.DB 0x00, 0x00     ; X-coordinate of top-left corner of display
+.DB 0x00, 0x00     ; Y-coordinate of top-left corner of display
+.DB 0x00, 0x00     ; Width of display in pixels
+.DB 0x00, 0x00     ; Height of display in pixels
+.DB 0x00           ; End of command buffer
+
+; Length of command buffer
+cmd_length:
+.DB $ - cmd_vga16
+
+; SPI buffer for sending command
+spi_buffer:
+.DB 0x00
---
+++ CPM/fabGL/VGAController v3.asm
@@ -0,0 +1,21 @@
+; Set org to 100h for CP/M
+	org 100h
+
+	; Initialize the SPI interface
+	ld a, 4		; Set SPI speed to Fosc/4 (1 MHz at 4 MHz clock)
+	out (7), a	; Send control byte to SPI port
+	ld a, 64	; Set SPI control byte for master mode, MSB first, enable SPI
+	out (7), a	; Send control byte to SPI port
+
+	; Send the VGAController command
+	ld a, 0x80	; VGAController command code
+	out (7), a	; Send command code to SPI port
+
+	; Wait for transfer to complete
+wait:
+	in a, (6)	; Read status register from SPI port
+	and 0x80	; Mask out all but transfer complete bit
+	jr nz, wait	; Wait until transfer is complete
+
+	; End of program
+	ret
